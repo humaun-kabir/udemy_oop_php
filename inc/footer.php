75f12de12dd61b1ce0ec2669b1e8bf5e0356bbe8
Humaun Kabir <asmhumaunkabir@gmail.com>
2021-06-11 17:43:24 +0600

    Create footer.php

---
+++ inc/footer.php
@@ -0,0 +1,13 @@
+</div>
+
+  <footer class="footeroption">
+ 
+	  <section class="footerone">
+		  <p>Like us: facebook.com/humaun.deon</p>
+		   <p>eMail: asmhumaunkabir@gmail.com</p>
+		  <p>Web: www.w3schools.com</p>
+	  </section>
+  </footer>
+
+</body>
+</html>
